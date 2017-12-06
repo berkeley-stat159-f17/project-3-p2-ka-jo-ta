4f8fa35324d936a7aa8a540b82bc3e875037018e
Sarah Johnson <sarah_johnson@berkeley.edu>
2017-12-06 00:25:43 -0800

    Add two_way function
    Add two_way function to p3functions, which creates a two-way frequency
    table between two dataframe columns. Use it in data_explorations
    notebook to look at traveltime and reason, and add tests to tests.py.

--- p3functions.py
+++ p3functions.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def make_indicators(df, names):
     """Make indicator columns in dataframe df of whether existing columns are 
     equal to given values.
@@ -15,3 +18,23 @@ def make_indicators(df, names):
     """
     for k,v in names.items():
         df[k] = 1*(df[v[0]] == v[1])
+
+def two_way(df, col0, col1):
+    """Make two-way frequency table of two columns in a dataframe.
+
+    Args:
+        df (pandas.DataFrame): Dataframe containing columns to be compared.
+        col0 (String)        : Name of first column to compare.
+        col1 (String)        : Name of second column to compare.
+
+    Returns:
+        df (pandas.DataFrame): Dataframe of two-way frequency table.
+    """
+    index = np.sort(df[col0].unique())
+    columns = np.sort(df[col1].unique())
+    tab = pd.DataFrame(index=index, columns=columns)
+    for i in index:
+        for j in columns:
+            count = df[(df[col0] == i) & (df[col1] == j)].shape[0]
+            tab.loc[i,j] = count
+    return tab
